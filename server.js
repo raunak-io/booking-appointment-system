937e6ef4d46d68622c9cbc5b18a79b5f8ce72faa
raunak-io <fullstackerr@gmail.com>
2024-07-28 09:51:59 +0530

    initial project setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+
+process.on('uncaught Exception', err => {
+    console.log('uncaught exception shutting down ...');
+    console.log(err.name, err.message);
+    process.exit(1);
+  });
+
+  dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+console.log(app.get('env'));
+
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST ||'0.0.0.0';
+const server = app.listen(port,host, () => {
+  console.log(`server is lstening on port : ${port} `);
+});
+
+process.on('uncaught rejection', err => {
+  console.log('uncaught rejection shutting down');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
